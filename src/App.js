4191e8b3dc036e94cf38f4e26f06f67fb2860ff0
cli333 <cml8198@protonmail.com>
2019-12-23 16:28:12 -0800

    updated with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/Homepage";
+
+const SecretPage = ({
+  match: {
+    params: { id }
+  }
+}) => (
+  <div>
+    <h1>You've found {id}'s secret page!</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/secret/:id" component={SecretPage} />
+      </Switch>
     </div>
   );
 }
