7edcf522b5d9c4bede9b1a87c3156abdbf189488
cli333 <50061720+cli333@users.noreply.github.com>
2019-12-31 14:58:42 -0800

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,7 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const config = {
-  apiKey: "",
+  apiKey: "AIzaSyB320pNxRcvJs2Ids62wGUv_5HeKkBlDDE",
   authDomain: "crown-db-d4b4a.firebaseapp.com",
   databaseURL: "https://crown-db-d4b4a.firebaseio.com",
   projectId: "crown-db-d4b4a",
@@ -11,7 +11,7 @@ const config = {
   messagingSenderId: "382147981072",
   appId: "1:382147981072:web:cb0c091efe753af0cb7f95",
   measurementId: "G-5SKP5SJFQ6"
-};
+}
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
